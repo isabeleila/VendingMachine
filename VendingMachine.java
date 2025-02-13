21d526f2a3b7abe08325a98de53545b723653e4d
isabeleila <leilaiayers@gmail.com>
2025-02-12 19:44:12 -0500

    changes

--- VendingMachine.java
+++ VendingMachine.java
@@ -4,4 +4,6 @@ public class VendingMachine{
     private static final String[] Items = {"Snickers", "Skittles", "Cookies", "Granola Bar", "Hershey" };
     private static final double [] Prices = {1.50, 1.25, 2.0, 1.75, 2.0};
     public static final int[] Inventory = {4, 4, 4, 4, 4};
+
+    
 }
